6f9cc04b449a63b7b9c0192b96513ccc7b828471
default <bianca.bertinato@stu.ocsb.ca>
2025-12-11 01:42:43 +0000

    added word search

---
+++ main.ts
@@ -0,0 +1,23 @@
+/**
+ * @author Bianca Boo
+ * @version 1.0.0
+ * @date 2025-12-10
+ * @fileoverview This program uses a word search in a given sentence.
+ */
+
+console.log("Please enter a sentence?");
+let sentence: string = prompt("");
+
+console.log("Please enter a word to search for in your sentence?");
+let word: string = prompt("");
+
+// check if the word exists
+if (sentence.includes(word)) {
+  console.log("Hooray, the word " + word +
+    " was found in the sentence: " + sentence);
+} else {
+  console.log("Sorry, the word " + word +
+    " was not found in the sentence: " + sentence);
+}
+
+console.log("\nDone.");
